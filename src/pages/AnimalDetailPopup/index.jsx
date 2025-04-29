5ecfcd8f1bc31009d36cc40c2e12f97b36801b15
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-04-29 13:43:39 +0200

    uploading recent changes

--- src/pages/AnimalDetailPopup/index.jsx
+++ src/pages/AnimalDetailPopup/index.jsx
@@ -7,8 +7,14 @@ const AnimalDetailPopup = ({displayContent,closePopup}) => {
             <p>Name:{displayContent.name}</p>
             <p>LifeSpan:{displayContent.lifeSpan}</p>
             <div>Food:{displayContent.food.map((food, index) => <p key={index}>{food}</p>)}</div>
+            <div>Length: {displayContent.length}</div>
+            <div>Weight: {displayContent.weight}</div>
+            <div>Place: {displayContent.place}</div>
+            <div>GroupName: {displayContent.group}</div>
+            
         </div>
     )
 }
 
 export default AnimalDetailPopup;
+
